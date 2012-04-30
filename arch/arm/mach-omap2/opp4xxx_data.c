73dcf2232eb1a03d1ac6e24937ab21bd7e21845e
og <ogdobber@gmail.com>
2012-04-29 20:36:07 -0700

    drop gpu to 307

--- arch/arm/mach-omap2/opp4xxx_data.c
+++ arch/arm/mach-omap2/opp4xxx_data.c
@@ -153,11 +153,11 @@ static struct omap_opp_def __initdata omap443x_opp_def_list[] = {
 	OPP_INITIALIZER("aess", "abe_clk", "iva", true, 196608000, OMAP4430_VDD_IVA_OPP100_UV),
 };
 
-#define OMAP4460_VDD_MPU_OPP50_UV		925000
-#define OMAP4460_VDD_MPU_OPP100_UV		1050000
-#define OMAP4460_VDD_MPU_OPPTURBO_UV		1125000
-#define OMAP4460_VDD_MPU_OPPNITRO_UV		1250000
-#define OMAP4460_VDD_MPU_OPPOC_UV		1300000
+#define OMAP4460_VDD_MPU_OPP50_UV		850000
+#define OMAP4460_VDD_MPU_OPP100_UV		975000
+#define OMAP4460_VDD_MPU_OPPTURBO_UV		1050000
+#define OMAP4460_VDD_MPU_OPPNITRO_UV		1175000
+#define OMAP4460_VDD_MPU_OPPOC_UV		1250000
 
 struct omap_volt_data omap446x_vdd_mpu_volt_data[] = {
 	VOLT_DATA_DEFINE(OMAP4460_VDD_MPU_OPP50_UV, OMAP44XX_CONTROL_FUSE_MPU_OPP50, 0xf4, 0x0c, OMAP_ABB_NOMINAL_OPP),
@@ -263,7 +263,7 @@ static struct omap_opp_def __initdata omap446x_opp_def_list[] = {
 	/* SGX OPP2 - OPP100 */
 	OPP_INITIALIZER("gpu", "dpll_per_m7x2_ck", "core", true, 307200000, OMAP4460_VDD_CORE_OPP100_UV),
 	/* SGX OPP3 - OPPOV */
-	OPP_INITIALIZER("gpu", "dpll_per_m7x2_ck", "core", true, 384000000, OMAP4460_VDD_CORE_OPP100_OV_UV),
+	OPP_INITIALIZER("gpu", "dpll_per_m7x2_ck", "core", false, 384000000, OMAP4460_VDD_CORE_OPP100_OV_UV),
 	/* FDIF OPP1 - OPP25 */
 	OPP_INITIALIZER("fdif", "fdif_fck", "core", true, 32000000, OMAP4460_VDD_CORE_OPP50_UV),
 	/* FDIF OPP2 - OPP50 */
